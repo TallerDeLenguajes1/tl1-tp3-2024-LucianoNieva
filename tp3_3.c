582c3a32a9b7eb931164744ce49816faf7fb4690
Luciano Fabrizio Nieva <Lucianonieva49@gmai.com>
2024-04-17 09:28:31 -0300

    punto 5 finalizado

--- tp3_3.c
+++ tp3_3.c
@@ -1,16 +1,20 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#define cantNombre 5
+
 
 
 int main(){
+int cantidad;
+printf("Ingrese la cantidad de nombres que quiere ingresar: ");
+scanf("%d",&cantidad);
 
-char ** nombre = malloc(cantNombre*sizeof(char *));
+char ** nombre = malloc(cantidad*sizeof(char *));
 char * buff = malloc(100*sizeof(char));
 
-for (int i = 0; i < cantNombre; i++)
+for (int i = 0; i < cantidad; i++)
 {
+    fflush(stdin);
     printf("Ingresa un nombre: \n");
     gets(buff);
     nombre[i] = malloc((strlen(buff)+1)*sizeof(char));
@@ -18,10 +22,12 @@ for (int i = 0; i < cantNombre; i++)
     
 }
 
-for (int i = 0; i < cantNombre; i++)
+for (int i = 0; i < cantidad; i++)
 {
     puts(nombre[i]);
 }
 
+free(nombre);
+free(buff);
    return 0;
 }
